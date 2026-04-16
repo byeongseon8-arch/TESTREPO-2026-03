ba3a09b75122cbbb05d8c247cf1f09e2296d82ab
byeongseon8-arch <byeongseon8@gamil.com>
2026-04-16 17:13:56 +0900

    2026. 04. 16.

---
+++ JAVA01/src/Ch01/C03음수.java
@@ -0,0 +1,17 @@
+package Ch01;
+
+public class C03음수 {
+
+	public static void main(String[] args) {
+
+	// 컴퓨터(CPU)는 구조상 덧셈연산을 할 수 있다 (O)
+	// 컴퓨터(CPU)는 구조상 뺄셈연산을 할 수 있다 (X)
+	// 보수 개념 + 가산처리를 통해서 뺄셈 결과를 만들어 낸다 (o)
+		
+	
+		
+		
+		
+	}
+
+}
